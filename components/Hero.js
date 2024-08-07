098248994a7b2c161401b734bf2241140a3cd9a6
kai-hayamizu <67088325+kai-kondo@users.noreply.github.com>
2024-08-07 14:55:01 +0900

    Hero画面修正

--- components/Hero.js
+++ components/Hero.js
@@ -34,46 +34,67 @@ const Hero = ({
           className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
           variants={scrollAnimation}
         >
-          <div className=" flex-col justify-center items-start row-start-2 sm:row-start-1">
-            <div className="mr-2 relative w-40 h-30 sm:w-72 sm:h-45 lg:w-96 lg:h-60">
-              <Image
-                src="/assets/Hero_logo.png"
-                alt="VPN Illustrasi"
-                quality={100}
-                layout="fill"
-                objectFit="contain"
-              />
-            </div>
-            <div className="flex flex-col sm:flex-row items-center sm:ml-12">
-              <p className="text-black-600 mt-8 mb-1 sm:ml-5">
-                軽貨物配送業の経営をラクに
-              </p>
-            </div>
-
-            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal mt-1 mb-6 ml-12">
-              <strong>ケイラクラウド</strong>
+          <div className=" flex-col justify-center items-start row-start-1 sm:row-start-1">
+            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
+              軽貨物配送業の経営をラクに
+              <strong className="text-orange-500 text-4xl lg:text-5xl xl:text-6xl">
+                ケイラクラウド
+              </strong>
             </h1>
-            <div className="ml-12">
-              <LinkScroll
-                to="contact"
-                smooth={true}
-                duration={500}
-                offset={-150} // オフセットを調整して、スクロール位置を微調整
-              >
-                <ButtonPrimary>無料で始めてみる</ButtonPrimary>
-              </LinkScroll>
+            <div className="flex flex-col sm:ml-12 border border-gray-100 p-4 shadow-xl rounded-lg ring-2 ring-gray-500 mt-8">
+              <div className="font-bold flex flex-col sm:flex-row items-center text-center">
+                <div className="rounded-full bg-gray-100 w-32 h-32 border-2 border-gray-100 flex items-center justify-center mr-4">
+                  <p className="text-xs">
+                    ドライバー5名<br></br>元請け企業2社まで<br></br>
+                    <strong className="text-sm font-bold text-green-500">
+                      ずっと無料！
+                    </strong>
+                  </p>
+                </div>
+                <div className="flex flex-col items-start sm:items-start">
+                  <p className="text-black-600 mt-2 mb-1 text-xl">
+                    <strong className="text-4xl text-green-500">
+                      全ての機能
+                    </strong>
+                    が
+                  </p>
+                  <p className="text-black-600 mt-2 mb-1 text-xl">
+                    <strong className="text-5xl text-green-500">無料</strong>
+                    で使える！
+                  </p>
+                </div>
+              </div>
+
+              <div className="ml-12 mt-12 mb-1 sm:ml-24">
+                <LinkScroll
+                  to="contact"
+                  smooth={true}
+                  duration={500}
+                  offset={-150} // オフセットを調整して、スクロール位置を微調整
+                >
+                  <ButtonPrimary>無料で始めてみる</ButtonPrimary>
+                </LinkScroll>
+              </div>
             </div>
           </div>
           <div className="flex w-full">
             <motion.div className="h-full w-full" variants={scrollAnimation}>
               <Image
-                src="/assets/hero2.svg"
+                src="/assets/test2.png"
                 alt="VPN Illustrasi"
                 quality={100}
-                width={612}
-                height={383}
+                width={48000} // 幅をさらに大きく変更
+                height={32000} // 高さをさらに大きく変更
                 layout="responsive"
               />
+              <p className="text-center mt-4 font-bold text-black-600">
+                <span className="underline text-blue-500 shadow-xl text-xl">
+                  <strong className="text-4xl text-green-500">
+                    インボイス制度
+                  </strong>
+                  に対応！
+                </span>
+              </p>
             </motion.div>
           </div>
         </motion.div>
@@ -88,7 +109,7 @@ const Hero = ({
               variants={scrollAnimation}
             >
               <div className="flex mx-auto w-40 sm:w-auto">
-                <div className="flex items-center justify-center bg-orange-100 w-12 h-12 mr-6 rounded-full">
+                <div className="flex items-center justify-center bg-orange-500 w-12 h-12 mr-6 rounded-full">
                   <img src={listUsers.icon} className="h-6 w-6" />
                 </div>
                 <div className="flex flex-col">
